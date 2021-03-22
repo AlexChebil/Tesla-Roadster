1910951bbeaefab5d87275176dc7268279821b34
ALEX <skanderchebil1@gmail.com>
2021-03-22 02:13:33 +0100

    main DONE !

--- JS/app.js
+++ JS/app.js
@@ -16,3 +16,28 @@ const observer = new IntersectionObserver(entries =>{
 animationObjects.forEach(animationObjects=>{
      observer.observe(animationObjects);
 })
+
+//-------------------------------------
+
+const image= document.querySelector('.topImg_2');
+const image2= document.querySelectorAll('.topImg_2');
+const trigger = document.querySelector('.trigger');
+
+trigger.addEventListener('mouseover', () =>{
+     image.classList.toggle('active');
+
+})
+
+const observeration = new IntersectionObserver(entries =>{
+     entries.forEach(entry=>{
+          if (entry.isIntersecting) {
+               
+          } else {
+               entry.target.classList.remove('active');
+          }
+     })
+})
+     
+image2.forEach(image2=>{
+     observeration.observe(image2);
+})
