5978c4409fb8bbe5df967ab1a626fa9475da652b
ALEX <skanderchebil1@gmail.com>
2021-05-14 20:34:57 +0100

    started to work on a new , much better and correct method of mediaQ using Firefox Devs ! (working across all browsers not only chrome)

--- JS/app.js
+++ JS/app.js
@@ -1,38 +1,38 @@
 const animationObjects = document.querySelectorAll('.animationConcerned');
 const options = {
-     rootMargin: '100px' ,
+     rootMargin: '100px',
      threshold: 0.1
-   }
+}
 
-const observer = new IntersectionObserver(entries =>{
-     entries.forEach(entry=>{
+const observer = new IntersectionObserver(entries => {
+     entries.forEach(entry => {
           if (entry.isIntersecting) {
                entry.target.classList.add('anim');
-               
+
           }
-          else{
+          else {
                entry.target.classList.remove('anim');
 
           }
      })
 }, options)
 
-animationObjects.forEach(animationObjects=>{
+animationObjects.forEach(animationObjects => {
      observer.observe(animationObjects);
 })
 //-----------------------------------------------------------------
 const topImage1 = document.getElementById('topImg_1');
-const topImageObserver = new IntersectionObserver(entries=>{
-     entries.forEach(entry=>{
+const topImageObserver = new IntersectionObserver(entries => {
+     entries.forEach(entry => {
           if (entry.isIntersecting) {
                topImage1.classList.add('animTopImage1');
           }
-          else{
+          else {
                topImage1.classList.remove('animTopImage1');
           }
      })
 })
-  
+
 topImageObserver.observe(topImage1);
 
 
@@ -41,23 +41,23 @@ topImageObserver.observe(topImage1);
 
 const trigger = document.querySelectorAll('.trigger');
 
-let optionsIO={
+let optionsIO = {
      rootMargin: '100px',
 }
 
 
-const observerrr = new IntersectionObserver(entries=>{
-          entries.forEach(entry=>{
-               if (entry.isIntersecting) {
-                    entry.target.classList.add('active');
-               }
-               else{
-                    entry.target.classList.remove('active');
-               }
-          })
-     },optionsIO)
+const observerrr = new IntersectionObserver(entries => {
+     entries.forEach(entry => {
+          if (entry.isIntersecting) {
+               entry.target.classList.add('active');
+          }
+          else {
+               entry.target.classList.remove('active');
+          }
+     })
+}, optionsIO)
 
-trigger.forEach(trigger=>{
+trigger.forEach(trigger => {
      observerrr.observe(trigger);
 })
 
@@ -73,10 +73,21 @@ const hamburger = document.getElementById('hamburgerContainer');
 const nav = document.querySelectorAll('.navDivs');
 const wall = document.getElementById('wall');
 
-     hamburger.addEventListener('click', () =>{
-          wall.classList.toggle('active'); 
-          for (let index = 0; index < nav.length; index++) {
-               nav[index].classList.toggle('active');
-          }
-               
-     })
+hamburger.addEventListener('click', () => {
+     wall.classList.toggle('active');
+     for (let index = 0; index < nav.length; index++) {
+          nav[index].classList.toggle('active');
+     }
+
+})
+
+
+
+
+
+
+for (let index = 0; index < 10; index++) {
+     console.log(index);
+     console.log('khami');
+
+}
